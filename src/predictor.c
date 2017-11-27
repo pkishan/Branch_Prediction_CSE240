a404f4742cb2ee0d18214645792cbc7532dc301f
Sai Kishan Pampana <spampana@ucsd.edu>
2017-11-27 15:11:30 -0800

    Finished code for gshare

--- src/predictor.c
+++ src/predictor.c
@@ -37,7 +37,9 @@ int verbose;
 
 uint8_t *PHT;		// Defining this way would be helping in shifting bits
 uint32_t ghistoryReg;	// The global pattern will be stored here
-
+uint32_t mask;		// We will use this to select thre required number of bits
+int count = 0;		// Used for debugging
+unsigned int index;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
@@ -49,34 +51,38 @@ uint32_t ghistoryReg;	// The global pattern will be stored here
 
 uint8_t gshare(uint32_t pc)
 {
-	//uint32_t mask = ghistoryReg;
-  	//// printf("The value of PC = %x\n", pc);
-  	//pc = pc&mask;
-  	//uint8_t pc_lsb = pc;
-  	//uint8_t index = pc_lsb ^ ghistoryBits;
-  	// if(PHT[index] == 0 || PHT[index] == 1)
-  	//   return NOTTAKEN;
-  	// else
-  	//   return TAKEN;
-  	return NOTTAKEN;
+	//printf("Entered this function gshare\n");
+  	uint32_t temp = pc^ghistoryReg;
+	index = temp&mask;	
+	if(PHT[index] == 0 || PHT[index] == 1)
+		return NOTTAKEN;
+	else
+		return TAKEN;
 }
 
 void update_gshare(uint32_t pc, uint8_t outcome)
 {
-  // uint32_t mask = 0x000000FF;
-  // pc = pc&mask;
+	//printf("Entered the function update_gshare\n");
+	//printf("The value of pc = %d\n", pc);
+	if(outcome == 0)
+		count++;
+	//printf("The value of count = %d\n", count);
+	//printf("Flag\n");
+  	if(outcome)
+  	{
+  	  if(PHT[index] != ST)
+  	    PHT[index]++;
+  	}
 
-  // if(outcome)
-  // {
-  //   if(PHT[pc] != 3)
-  //     PHT[pc]++;
-  // }
-
-  // else
-  // {
-  //   if(PHT[pc] != 0)
-  //     PHT[pc]--;
-  // }
+  	else
+  	{
+  	  if(PHT[index] != SN)
+  	    PHT[index]--;
+  	}
+	//printf("Flag 2\n");
+	
+	ghistoryReg = ghistoryReg << 1;
+	ghistoryReg += outcome;	
 
 }
 
@@ -101,8 +107,11 @@ init_predictor()
  		case STATIC:
  			break;	
  		case GSHARE:
- 			//PHT = malloc(pow(2, ghistoryBits)*sizeof(uint8_t));
-            		//memset(PHT, 1, (1 << ghistoryBits) * sizeof(uint8_t));
+			mask = pow(2, ghistoryBits) - 1;
+			printf("The value of mask = %d \n", mask);
+ 			PHT = malloc(pow(2, ghistoryBits)*sizeof(uint8_t));
+            		memset(PHT, WN,pow(2,ghistoryBits)*sizeof(uint8_t));	// Initiating as weakly not taken 
+			ghistoryReg = 0;
             		break;
 		case TOURNAMENT:
 			break;
@@ -118,53 +127,52 @@ init_predictor()
 // Returning TAKEN indicates a prediction of taken; returning NOTTAKEN
 // indicates a prediction of not taken
 //
-uint8_t
-make_prediction(uint32_t pc)
+uint8_t make_prediction(uint32_t pc)
 {
   //
   //TODO: Implement prediction scheme
   //
 
   // Make a prediction based on the bpType
-  switch (bpType) {
-    case STATIC:
-      return NOTTAKEN;
-    case GSHARE:
-      return gshare(pc);
-      break;
-    case TOURNAMENT:
-    case CUSTOM:
-    default:
-      break;
-  }
+	switch (bpType)
+	{
+		case STATIC:
+			return NOTTAKEN;
+  	  	case GSHARE:
+  	  		return gshare(pc);
+	  	  	break;
+  	  	case TOURNAMENT:
+  	  	case CUSTOM:
+  	  	default:
+			break;
+  	}
 
   // If there is not a compatable bpType then return NOTTAKEN
-  return NOTTAKEN;
+	return NOTTAKEN;
 }
 
 // Train the predictor the last executed branch at PC 'pc' and with
 // outcome 'outcome' (true indicates that the branch was taken, false
 // indicates that the branch was not taken)
 //
-void
-train_predictor(uint32_t pc, uint8_t outcome)
+void train_predictor(uint32_t pc, uint8_t outcome)
 {
   //
   //TODO: Implement Predictor training
   //
-  switch (bpType)
-  {
-    case STATIC:
-      break;
-    case GSHARE:
-      update_gshare(pc, outcome);
-      break;
-    case TOURNAMENT:
-      break;
-    case CUSTOM:
-      break;
-    default:
-      break;  
-  }
+	switch (bpType)
+  	{
+		case STATIC:
+			break;
+		case GSHARE:
+			update_gshare(pc, outcome);
+			break;
+		case TOURNAMENT:
+			break;
+		case CUSTOM:
+			break;
+		default:
+			break;  
+  	}
   
 }
